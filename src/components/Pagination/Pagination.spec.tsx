0feb43718c797b294b40eef1fa0d9c19386aaabf
Matheus <matheus.bnaguiar@gmail.com>
2022-09-05 11:24:20 -0300

    chore: Adjustments on test

--- src/components/Pagination/Pagination.spec.tsx
+++ src/components/Pagination/Pagination.spec.tsx
@@ -10,7 +10,7 @@ const TestCase = ({ quantity = 1, active = 1, range = 2 }) => {
 
 test("Don't show anything when there is no pages", async () => {
   const { findByText } = render(<TestCase quantity={0} />);
-  await waitFor(() => expect(findByText('0')).not.toBe());
+  await waitFor(() => expect(findByText('0')).not.toBe(true));
 });
 
 test('Show the page indicator', async () => {
@@ -21,18 +21,24 @@ test('Show the page indicator', async () => {
 test('Show the current page correctly', async () => {
   const { queryByText } = render(<TestCase quantity={5} />);
   const paginationItem = queryByText('2');
-  fireEvent.click(paginationItem);
-  expect(paginationItem.getAttribute('aria-current')).toBe('true');
+  if (paginationItem) {
+    fireEvent.click(paginationItem);
+    expect(paginationItem.getAttribute('aria-current')).toBe('true');
+  }
 });
 
 test('Show pages in the middle, last and first', async () => {
   const { queryByText } = render(<TestCase quantity={10} range={3} />);
 
   const paginationItem4 = queryByText('4');
-  fireEvent.click(paginationItem4);
+  if (paginationItem4) {
+    fireEvent.click(paginationItem4);
+  }
 
   const paginationItem5 = queryByText('5');
-  fireEvent.click(paginationItem5);
+  if (paginationItem5) {
+    fireEvent.click(paginationItem5);
+  }
 
   queryByText('1');
   queryByText('3');
@@ -46,11 +52,15 @@ test("Don't show pages that are not on range", async () => {
   const { queryByText } = render(<TestCase quantity={10} range={3} />);
 
   const paginationItem4 = queryByText('4');
-  fireEvent.click(paginationItem4);
+  if (paginationItem4) {
+    fireEvent.click(paginationItem4);
+  }
 
   const paginationItem5 = queryByText('5');
-  fireEvent.click(paginationItem5);
+  if (paginationItem5) {
+    fireEvent.click(paginationItem5);
+  }
 
-  await waitFor(() => expect(queryByText('2')).not.toBe());
-  await waitFor(() => expect(queryByText('8')).not.toBe());
+  await waitFor(() => expect(queryByText('2')).not.toBe(true));
+  await waitFor(() => expect(queryByText('8')).not.toBe(true));
 });
